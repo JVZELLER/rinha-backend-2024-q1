e4dda841bc27e34a8e9e0af1b3ac42e2f9400451
José Victor Zeller <josevictorzg@gmail.com>
2024-02-18 15:48:33 -0300

    fix: insert_entry database function

--- priv/repo/migrations/20240217195632_create_insert_entry_function.exs
+++ priv/repo/migrations/20240217195632_create_insert_entry_function.exs
@@ -1,44 +1,55 @@
 defmodule RinhaBackend.Repo.Migrations.CreateInsertEntryFunction do
   use Ecto.Migration
 
-  def change do
+  def up do
     repo().query!(~s/
-      CREATE OR REPLACE FUNCTION insert_entry(
-          entry_type varchar(1),
-          client_id bigint,
-          entry_amount bigint
-      ) RETURNS void AS
-      $$
-      BEGIN
-        with updated_client_data as (
-          update clients
-          set balance = clients.balance + case when entry_type = 'd' then entry_amount * -1 else entry_amount end
-          where where id = client_id;
-          returning balance, limit;
-        ), inserted_entry as
-          INSERT INTO entries (type, client_id, amount, inserted_at) VALUES (entry_type, client_id, entry_amount, now());
-        )
+        CREATE OR REPLACE FUNCTION fn_insert_entry(
+            entry_amount bigint,
+            entry_type varchar(1),
+            entry_description varchar(10),
+            client_id int8
+        ) RETURNS jsonb AS
+        $$
+        DECLARE
+        client_limit bigint;
+        client_balance bigint;
+        BEGIN
+          -- Updates client balance
+          WITH updated_client_data AS (
+            UPDATE clients
+            SET balance = clients.balance + entry_amount
+            WHERE id = client_id
+            RETURNING balance, "limit"
+          )
+          SELECT balance, "limit"
+          FROM updated_client_data
+          INTO client_balance, client_limit;
 
 
-
-
-          -- Check if the entry type is 'd' (debt)
-          IF entry_type = 'd' THEN
-              -- Check if the entry amount exceeds the client's limit
-              IF entry_amount - (SELECT balance FROM clients WHERE id = client_id) < (SELECT limit FROM clients WHERE id = client_id) * -1 THEN
-                  RAISE EXCEPTION 'Entry amount exceeds client limit!';
-              END IF;
+          -- Check if client exists
+          IF client_balance IS NULL OR client_limit IS NULL THEN
+            RAISE EXCEPTION 'client_not_found';
           END IF;
 
-          -- Insert the entry into the "entries" table
-          INSERT INTO entries (type, client_id, amount) VALUES (entry_type, client_id, entry_amount);
+          -- Check client has invalid balance
+          IF client_limit  * -1 > client_balance THEN
+            RAISE EXCEPTION 'entry_amount_exceeds_client_limit';
+          END IF;
 
-          -- Update the client's balance accordingly
-          UPDATE clients SET balance = balance + entry_amount WHERE id = client_id;
-      END;
-      $$
-      LANGUAGE plpgsql;
+          -- Insert entry
+          INSERT INTO entries (amount, type, description, client_id, inserted_at) VALUES (entry_amount, entry_type, entry_description, client_id, now());
 
+          -- Return client balance and limit as JSONB
+          RETURN jsonb_build_object('limit', client_limit, 'balance', client_balance);
+        END;
+        $$
+        LANGUAGE plpgsql;
+      /)
+  end
+
+  def down do
+    repo().query!(~s/
+      DROP FUNCTION fn_insert_entry (bigint, varchar(1), varchar(10), int8);
     /)
   end
 end
