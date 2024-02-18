0266d7ed498bf0c5db5e14795e2a5c72621efa41
José Victor Zeller <josevictorzg@gmail.com>
2024-02-18 15:49:04 -0300

    fix: entry amount accordingly to entry type

--- lib/rinha_backend/schemas/entry.ex
+++ lib/rinha_backend/schemas/entry.ex
@@ -5,7 +5,8 @@ defmodule RinhaBackend.Schemas.Entry do
   defstruct ~w(id amount type description client_id inserted_at)a
 
   defguard valid_type(t) when t in ~w(c d C D)
-  defguard valid_description(description) when length(description) <= 10 and description != ""
+
+  defguard valid_description(description) when byte_size(description) <= 10 and description != ""
 
   @type t :: %__MODULE__{
           id: integer(),
@@ -22,7 +23,9 @@ defmodule RinhaBackend.Schemas.Entry do
              valid_type(type) and
              valid_description(desc) and
              is_integer(client_id) do
-    struct(__MODULE__, params)
+    amount = if(type == "d", do: -amount, else: amount)
+
+    struct(__MODULE__, %{params | amount: amount})
   end
 
   def new(_invalid), do: {:error, :invalid_args}
