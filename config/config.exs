20b8ff7f3b08b5a2a4109a575e6adc84cad0c7ec
José Victor Zeller <josevictorzg@gmail.com>
2024-02-25 18:05:23 -0300

    chore: improve application config

--- config/config.exs
+++ config/config.exs
@@ -6,7 +6,12 @@ config :rinha_backend, RinhaBackend.Repo,
   database: "rinha",
   username: "admin",
   password: "123",
-  hostname: "localhost"
+  hostname: "localhost",
+  pool_size: 100,
+  parameters: [application_name: "rinha_backend"]
+
+# Configures Elixir's Logger
+config :logger, :console, format: "$time $metadata[$level] $message\n"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
